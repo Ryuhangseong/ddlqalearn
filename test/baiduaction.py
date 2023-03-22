46ba7b1c017d72c365c9df7e8c4b0cb32ea0d719
ryuhangseong <liuhangcheng2002@gmail.com>
2023-03-22 19:46:18 +0800

    0.0.1 version -- 2023/3/22

---
+++ test/baiduaction.py
@@ -0,0 +1,26 @@
+'''
+Author: ryuhangseong liuhangcheng2002@gmail.com
+Date: 2023-03-22 16:31:04
+LastEditors: ryuhangseong liuhangcheng2002@gmail.com
+LastEditTime: 2023-03-22 17:51:23
+FilePath: \ddlqalearn\test\baiduaction.py
+Description: 
+
+Copyright (c) 2023 by liuhangcheng2002@gmail.com, All Rights Reserved. 
+'''
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from sele_po.elementoperator import ElementOperator
+
+class Action(ElementOperator):
+    
+    def __init__(self):
+        super(Action, self).__init__(self)
+
+if __name__ == '__main__':
+    action = Action()
+    action.test()
